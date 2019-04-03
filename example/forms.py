6a3445fd81e6434ce1e4f60c931f96e6f7a73aba
Maciej Krol <nitz@o2.pl>
2019-04-03 13:27:37 +0200

    Add form validation for name

--- example/forms.py
+++ example/forms.py
@@ -6,3 +6,11 @@ class GiftListForm(forms.ModelForm):
     class Meta:
         model = GiftList
         exclude = ("modified",)
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        if "curse" in name.lower():
+            raise forms.ValidationError("You can not use forbidden words")
+
+        # Always return the cleaned data, whether you have changed it or not.
+        return name
