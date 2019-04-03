71f4c2210452040983be00b81eef9fec5d991e37
Maciej Krol <nitz@o2.pl>
2019-04-03 12:30:38 +0200

    Add hello endpoint

--- php_advanced/urls.py
+++ php_advanced/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from example import views as example_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r"^hello$", example_views.hello_world),
+    url(r"^admin/", admin.site.urls),
 ]
