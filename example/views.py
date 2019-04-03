585c68bd1c6efb84fdbd8f1e0c137956897ac531
Maciej Krol <nitz@o2.pl>
2019-04-03 12:39:14 +0200

    Add index template

--- example/views.py
+++ example/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def hello_world(request):
@@ -7,3 +8,7 @@ def hello_world(request):
 
 def hello_name(request, name):
     return HttpResponse(f"Hello, {name}!")
+
+
+def hello_world_template(request):
+    return render(request, "index.html", {})
