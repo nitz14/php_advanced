f06a32f7c639538273388a4f96a17840394c8f1b
Maciej Krol <nitz@o2.pl>
2019-04-03 11:39:09 +0200

    Add Gift List Model

--- example/models.py
+++ example/models.py
@@ -1,3 +1,11 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class GiftList(models.Model):
+    name = models.CharField(max_length=64)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
